7957385ba1c8f0669b568e931e1db4638e9da383
KondratievArtem <kondrativ41@gmail.com>
2022-05-13 14:32:39 +0300

    add funkcion burger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+
+// функционал на всплытие меню на каталоге
 document.querySelector('.navbar__title-open').onclick = () => {
     document.querySelector('.navbar__manu').classList.toggle('activ');
 };
+
+// функционал для меню бургера
+
+document.querySelector('.logo__open-burger').onclick = () => {
+    document.querySelector('.br-menu').classList.add('activ');
+};
+document.querySelector('.br-menu__close').onclick = () => {
+    document.querySelector('.br-menu').classList.remove('activ');
+}
