56e4d214b25fbf0c89c1f6e5b6b018dd6ba94d9d
KondratievArtem <kondrativ41@gmail.com>
2022-05-13 21:29:27 +0300

    add block main__header

--- js/script.js
+++ js/script.js
@@ -22,3 +22,34 @@ document.querySelectorAll('.box-menu__header').forEach(item => {
         });
     };
 });
+
+document.querySelectorAll('.box-menu__item').forEach(item => {
+    item.onclick = function () {
+        let itemText = this.innerHTML,
+            select = this.closest('.box-menu__select');
+        textCurrent = this.closest('.box-menu__select').querySelector('.box-menu__current');
+        textCurrent.innerHTML = itemText;
+        select.classList.remove('activ');
+    };
+});
+
+// функционал для таймера времени
+
+// создание времени одчета==============================
+// получение времени от которого наченаеться отчет======
+let date = new Date(2022, 1, 24, 5).getTime();
+// обновление таймера=============
+let countDowenFunction = setInterval(function () {
+    // время на данный момент
+    let dateNew = new Date().getTime();
+    // промежуток времени
+    let distance = dateNew - date;
+    // высчитываем время
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    // результат
+    document.querySelector('.header-main__dey').innerHTML = days;
+    document.querySelector('.header-main__hors').innerHTML = hours;
+    document.querySelector('.header-main__minut').innerHTML = minutes;
+}, 1000)
